56d1a487bd2c87d49e046147a1a4d3fb86bc6124
Jahred Hope <jahred.hope@xero.com>
2016-05-29 15:23:40 +1000

    Add tests and linting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function getFileExtension(path) {
   return path.split('.').pop();
 }
 
-module.exports = function(content) {
+module.exports = function base64ImageLoader(content) {
   this.cacheable && this.cacheable();
   return `module.exports = "data:image/${getFileExtension(this.resourcePath)};base64,${content.toString('base64')}"`;
 };
---
+++ test/index.js
@@ -0,0 +1,42 @@
+/* eslint-env node, mocha */
+
+const assert = require('chai').assert;
+const base64ImageLoader = require('../index');
+
+const fs = require('fs');
+
+
+describe('testImageBase64', () => {
+  it('output the base64 string', () => {
+    const thisObj = { resourcePath: 'filename.png' };
+    const aBase64String = 'SGVsbG8gV29ybGQ=';
+    const result = base64ImageLoader.call(thisObj, new Buffer(aBase64String, 'base64'));
+
+    assert.include(result, aBase64String);
+  });
+
+  it('should set extension for jpg', () => {
+    const thisObj = { resourcePath: 'filename.jpg' };
+    const result = base64ImageLoader.call(thisObj, new Buffer(''));
+
+    assert.include(result, 'image/jpg');
+  });
+
+  it('should set extension for png', () => {
+    const thisObj = { resourcePath: 'filename.png' };
+    const result = base64ImageLoader.call(thisObj, new Buffer(''));
+
+    assert.include(result, 'image/png');
+  });
+
+  it('output the base64 of the test image', () => {
+    const testImage = fs.readFileSync('./test/1x1.png');
+    const testImageBase64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/j//z8ABf4C/qc1gYQAAAAASUVORK5CYII='; // eslint-disable-line max-len
+
+    const thisObj = { resourcePath: 'filename.png' };
+
+    const result = base64ImageLoader.call(thisObj, testImage);
+
+    assert.include(result, testImageBase64);
+  });
+});
