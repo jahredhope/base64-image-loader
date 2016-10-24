52a17d77f79212b4cdefda0fea37d8a6a79a47d4
Oleg Klimenko <o.klimenko@2gis.ru>
2016-10-24 19:51:35 +0700

    Fix jpeg and svg mime types

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 'use strict';
 
-function getFileExtension(path) {
-  return path.split('.').pop();
+function getMime(path) {
+  var extension = path.split('.').pop();
+  if (['svg', 'jpg', 'jpeg', 'webp', 'gif', 'png'].indexOf(extension) === -1) {
+    throw new Error('Unsupported type of image');
+  }
+  
+  if (extension == 'svg') {
+    return 'image/svg+xml';
+  }
+  
+  if (extension == 'jpg') {
+    return 'image/jpeg';
+  }
+  
+  return 'image/' + extension;
 }
 
 module.exports = function base64ImageLoader(content) {
   this.cacheable && this.cacheable();
-  return `module.exports = "data:image/${getFileExtension(this.resourcePath)};base64,${content.toString('base64')}"`;
+  return `module.exports = "data:${getMime(this.resourcePath)};base64,${content.toString('base64')}"`;
 };
 module.exports.raw = true;
